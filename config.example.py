074f55307517b8e0b735c3b32e4daa182363cec1
Artukryp <isark3@hotmail.com>
2025-07-06 16:19:58 -0600

    feat: Added config template file

---
+++ config.example.py
@@ -0,0 +1,17 @@
+# config.py
+
+# Telegram
+TOKEN = ''
+CHAT_ID = ''
+
+# Tesla
+TESLA_EMAIL = ''
+# 
+ZONA_PERMITIDA = ()
+RADIO_MAX_KM = 0.01  # en kilómetros
+
+# Autenticación manual solo si se requiere
+AUTHORIZATION_URL_FINAL = None  # o pon el string completo si ya tienes la URL final
+
+# Path para el script temporizador
+SCRIPT_PATH = ""
