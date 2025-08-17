29babd512413dcb5ad36752dc4de3341724f82a1
artukryp <isark3@hotmail.com>
2025-08-17 03:25:17 -0600

    Added interval time of execution

--- tesla-tracker-scheduler.py
+++ tesla-tracker-scheduler.py
@@ -5,6 +5,12 @@ from config import SCRIPT_PATH
 
 
 INTERVAL_MINUTES = 15
+START_HOUR = 0
+END_HOUR = 7
+
+def should_execute():
+    current_hour = datetime.now().hour
+    return START_HOUR <= current_hour < END_HOUR
 
 def ejecutar_script():
     print(f"🕒 Ejecutando Tesla-Tracker: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
@@ -15,7 +21,8 @@ def ejecutar_script():
 
 def main():
     while True:
-        ejecutar_script()
+        if should_execute():
+            ejecutar_script()
         time.sleep(INTERVAL_MINUTES * 60)
 
 if __name__ == "__main__":
